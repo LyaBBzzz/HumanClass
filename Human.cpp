21fdf2d10b9a405874d5f9d5d4cb52129041b297
Дмитрий Климанов <abunait@mail.ru>
2023-12-01 21:14:50 +1000

    Добавление свойств

--- Human.cpp
+++ Human.cpp
@@ -8,7 +8,11 @@ protected:
     string surname;
     int age;
     string gender;
-    
+
+public:
+    Human(const string& n, const string& s, int a, const string& g)
+        : name(n), surname(s), age(a), gender(g) {}
+
 };
 
 int main() {
